5e9d2b34de106ce4125de819d19b2a5a4c59097b
Scott Edlin <edlins@yahoo.com>
2020-03-10 15:21:56 -0600

    left out SERVER

--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-                <p>The server IP address is <?php echo $_['SERVER_ADDR']; ?>.</p>
+                <p>The server IP address is <?php echo $_SERVER['SERVER_ADDR']; ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
