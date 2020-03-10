d63e2f38651854094c6e338fc36d238319792d53
Scott Edlin <edlins@yahoo.com>
2020-03-10 15:01:17 -0600

    echo ip addr

--- src/index.php
+++ src/index.php
@@ -20,6 +20,8 @@
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
+                        $ip_server = $_SERVER['SERVER_ADDR']; 
+                        echo "Server IP Address is: $ip_server"; 
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
